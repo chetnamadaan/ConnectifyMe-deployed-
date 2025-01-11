ba6dbb2a8929677a3196e4577759309ccc837e12
chetnamadaan <chetna.madaan45@gmail.com>
2025-01-11 19:56:40 +0530

    Project completed

--- Backend/index.js
+++ Backend/index.js
@@ -4,9 +4,9 @@ import UserRoute from './Routes/User.route.js';
 import mongoose from 'mongoose';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
-
-import messageRoute from "./Routes/message.route.js"
-import {app, server} from "./SocketIO/server.js";
+import path from 'path';
+import messageRoute from './Routes/message.route.js';
+import { app, server } from './SocketIO/server.js';
 
 dotenv.config();
 
@@ -30,9 +30,22 @@ app.get('/', (req, res) => {
   res.send('Welcome to the API!');
 });
 
-// Routes for user
+// Routes for user and message
 app.use('/api/user', UserRoute);
 app.use('/api/message', messageRoute);
+
+// Code for production deployment
+if (process.env.NODE_ENV === 'production') {
+  const dirPath = path.resolve();
+
+  // Serving the static files from 'Frontend/dist' directory
+  app.use(express.static(path.join(dirPath, 'Frontend', 'dist')));
+
+  // Fallback for all other routes to serve index.html
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(dirPath, 'Frontend', 'dist', 'index.html'));
+  });
+}
 
 // Start server
 server.listen(PORT, () => {
